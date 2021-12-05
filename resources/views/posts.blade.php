6687a91d769fdbed15665668291415fee555f919
pradipta610 <pradipta.wistika@gmail.com>
2021-12-06 00:03:34 +0800

    fitur upload image part 2

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -25,7 +25,12 @@
     {{-- halaman posts all untuk card paling pertama atau paling gede --}}
     @if ($posts->count()>0)
     <div class="card mb-3">
-        <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        @if ($posts[0]->image)
+          <img src="/storage/{{ $posts[0]->image }}" alt="{{ $posts[0]->category->name }}" class="img-fluid">
+        @else
+          <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+        @endif
+        
         <div class="card-body text-center">
           <h3 class="card-title"><a href="/posts/{{ $posts[0]->slug }}"class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
         <p>
@@ -50,7 +55,12 @@
         <div class="col-md-4">
             <div class="card mb-3">
                 <div class="position-absolute px-3 py-2 rounded-bottom " style="background-color:rgba(0,0,0,0.8)"><a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a></div>
+                @if ($post->image)
+                 <img src="/storage/{{ $post->image }}" alt="{{ $post->category->name }}" class="img-fluid">
+                @else
                 <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                @endif
+                
                 <div class="card-body">
                   <h5 class="card-title"><a href="/posts/{{ $post->slug }}"class="text-decoration-none text-dark">{{ $post->title }}</a></h5>
                   <p>
