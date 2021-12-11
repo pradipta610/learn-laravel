3e6198f2f3c746c62e56be5492bb6db31fb976ae
pradipta610 <pradipta.wistika@gmail.com>
2021-12-11 14:15:57 +0800

    paralax home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,11 @@
 @extends('layout.transparentnavbar')
 @section('container')
   <section>
-      <h2 id="text"><span class="text1"> Welcome To My</span><br><span class="text2">BLOG</span> </h2>
-      <img src="img/matahari.png" alt="">
-      <img src="img/gedung.png" alt="">
-      <img src="img/saya.png" alt="">
+      <h2 id="text" ><span class="text1"> Welcome To My</span><br><span class="text2">BLOG</span> </h2>
+      <img src="img/gedung.png" alt="" id="gedung">
+      <img src="img/matahari.png" alt="" id="sun">
+      <img src="img/pesawat.png" alt="" id="plane">
+      <img src="img/saya.png" alt="" id="me">
 
   </section>
   <div class="sec">
@@ -17,5 +18,23 @@
           max-width: 100% !important;
           padding: 0px !important;
       }
+      .mt-3{
+          margin-top: 0 !important; 
+      }
   </style>
+  <script>
+      let gedung = document.getElementById('gedung');
+      let text = document.getElementById('text');
+      let sun = document.getElementById('sun');
+      let plane = document.getElementById('plane');
+      let me = document.getElementById('me');
+      window.addEventListener('scroll',function(){
+          let value = window.scrollY;
+          text.style.top = 10 + value * -0,9 + '%';
+          gedung.style.top = value * 0.5 + 'px';
+          sun.style.left = value * 0.1 + 'px';
+          plane.style.left = value * 0.5 + 'px';
+          me.style.left = value * 0.2 + 'px';
+      })
+  </script>
 @endsection
---
+++ resources/views/layout/mainhome.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/main.css">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    {{-- Bootstrap icon --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+   
+
+    <title>Home | {{ $title }}</title>
+  </head>
+  <body>
+  {{-- @yield('navbar') --}}
+  @yield('navbar')
+
+
+<div class="container mt-3">
+    @yield('container')
+</div>
+
+
+{{-- <div class="background">
+  <footer class="py-3 my-4 ">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Features</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2021 Company, Inc</p>
+  </footer>
+</div> --}}
+    <!--  Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+   
+  </body>
+</html>
